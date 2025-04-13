b0f960c537e21cc843d4166a27b3c544b7fe1cf5
*ST8x3Vryql <tranngoctien10304@gmail.com>
2025-04-13 23:22:29 +0700

    fix cors

--- server.js
+++ server.js
@@ -8,8 +8,21 @@ const cors = require('cors');
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://music-player-xi-pink.vercel.app',
+    'https://music-player-bphzyqstr-bancutewas-projects.vercel.app',
+    'https://music-player-git-admin-site-bancutewas-projects.vercel.app'
+];
+
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
